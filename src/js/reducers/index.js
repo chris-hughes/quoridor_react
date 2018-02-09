5985cadf81bd631341c474c0365cad2c44073e5f
chris-hughes-SB <chris.hughes@simplybusiness.co.uk>
2018-02-09 12:41:02 +0000

    basic pawn movement

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,21 +1,49 @@
 import * as types from "../constants/action_types";
 
-const initialState = {
+let initialState = {
   squares: Array(81).fill(null),
   blackIsNext: true
 }
+
+initialState.squares[4]="♙";
+initialState.squares[76]="♟";
+
+function getLegalMoves(cell){
+  let legalMoves=[]
+  if (cell % 9 === 0){
+    if (Math.floor(cell/9)===0) legalMoves.push(cell+1,cell+9)
+    else if (Math.floor(cell/9)===8) legalMoves.push(cell+1,cell-9)
+    else legalMoves.push(cell+1,cell+9,cell-9)
+  }
+  else if (cell % 9 === 8){
+    if (Math.floor(cell/9)===0) legalMoves.push(cell-1,cell+9)
+    else if (Math.floor(cell/9)===8) legalMoves.push(cell-1,cell-9)
+    else legalMoves.push(cell-1,cell+9,cell-9)
+  }
+  else legalMoves.push(cell+1,cell-1,cell+9,cell-9)
+
+  return legalMoves;
+}
+
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
 
     case types.MAKE_MOVE:
-        const squares = state.squares.slice();
-        squares[action.cell] = state.blackIsNext ? "♙" : "♟"
+      const squares = state.squares.slice();
+      const indexOfCurrentPlayer = state.blackIsNext ?
+        squares.indexOf("♟") : squares.indexOf("♙")
+      const legalMoves = getLegalMoves(indexOfCurrentPlayer);
 
-        return {
-          squares: squares,
-          blackIsNext: !state.blackIsNext
-        }
+      if (squares[action.cell] || legalMoves.indexOf(action.cell)===-1) return state;
+
+      squares[action.cell] = state.blackIsNext ? "♟" : "♙"
+      squares[indexOfCurrentPlayer] = null
+
+      return {
+        squares: squares,
+        blackIsNext: !state.blackIsNext
+      }
 
     default:
       return state
