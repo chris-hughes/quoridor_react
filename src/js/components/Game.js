49c4ac43bca45abae9f912e111f97f1e4e33dbf6
chris-hughes <hughes.c.i@gmail.com>
2018-02-10 13:09:27 +0000

    tidy up

--- src/js/components/Game.js
+++ src/js/components/Game.js
@@ -1,19 +1,36 @@
 import React, { Component } from "react"
+import { connect } from 'react-redux'
 import Board from './Board'
 
-class Game extends Component {
+const mapStateToProps = (state) => {
+  return {
+    blackIsNext: state.blackIsNext,
+    winner: state.winner
+  }
+}
+
+
+class defineGame extends Component {
   render(){
+    let status
+    if (this.props.winner){
+      status = 'Winner: '+ this.props.winner
+    } else {
+      status = 'Next player: ' + (this.props.blackIsNext ? 'Black' : 'White');
+    }
     return (
       <div className="game">
         <div className="game-board">
           <Board />
         </div>
         <div className="game-info">
-          <div>{/*status*/}</div>
+          <div>{status}</div>
         </div>
       </div>
     )
   }
 }
+
+const Game = connect(mapStateToProps)(defineGame);
 
 export default Game
