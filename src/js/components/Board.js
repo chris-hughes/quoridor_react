72b81f1f62fdc8908138a44dc4bea7a26ab6f399
chris-hughes <hughes.c.i@gmail.com>
2018-02-10 12:53:49 +0000

    walls enumerated

--- src/js/components/Board.js
+++ src/js/components/Board.js
@@ -22,54 +22,54 @@ class defineBoard extends Component {
     return <Square value={this.props.squares[i]} onClick={()=>this.props.makeMove(i)} />
   }
 
-  renderWall(orientation){
-    return <Wall class={orientation} onClick={()=>alert('wall')} />
+  renderWall(orientation,i){
+    return <Wall class={orientation} onClick={()=>console.log(i)} />
   }
 
-  renderBoardRow(j){
+  renderBoardRow(i){
     return (
       <div className="board-row">
-        {this.renderSquare(j*9+0)}
-        {this.renderWall('v-wall',0)}
-        {this.renderSquare(j*9+1)}
-        {this.renderWall('v-wall',0)}
-        {this.renderSquare(j*9+2)}
-        {this.renderWall('v-wall',0)}
-        {this.renderSquare(j*9+3)}
-        {this.renderWall('v-wall',0)}
-        {this.renderSquare(j*9+4)}
-        {this.renderWall('v-wall',0)}
-        {this.renderSquare(j*9+5)}
-        {this.renderWall('v-wall',0)}
-        {this.renderSquare(j*9+6)}
-        {this.renderWall('v-wall',0)}
-        {this.renderSquare(j*9+7)}
-        {this.renderWall('v-wall',0)}
-        {this.renderSquare(j*9+8)}
+        {this.renderSquare(i*9+0)}
+        {this.renderWall('v-wall',i*(8+9)+0)}
+        {this.renderSquare(i*9+1)}
+        {this.renderWall('v-wall',i*(8+9)+1)}
+        {this.renderSquare(i*9+2)}
+        {this.renderWall('v-wall',i*(8+9)+2)}
+        {this.renderSquare(i*9+3)}
+        {this.renderWall('v-wall',i*(8+9)+3)}
+        {this.renderSquare(i*9+4)}
+        {this.renderWall('v-wall',i*(8+9)+4)}
+        {this.renderSquare(i*9+5)}
+        {this.renderWall('v-wall',i*(8+9)+5)}
+        {this.renderSquare(i*9+6)}
+        {this.renderWall('v-wall',i*(8+9)+6)}
+        {this.renderSquare(i*9+7)}
+        {this.renderWall('v-wall',i*(8+9)+7)}
+        {this.renderSquare(i*9+8)}
       </div>
     )
   }
 
-  renderWallRow(k){
+  renderWallRow(i){
     return (
       <div className="wall-row">
-        {this.renderWall('h-wall',0)}
-        {this.renderWall('s-wall',0)}
-        {this.renderWall('h-wall',0)}
-        {this.renderWall('s-wall',0)}
-        {this.renderWall('h-wall',0)}
-        {this.renderWall('s-wall',0)}
-        {this.renderWall('h-wall',0)}
-        {this.renderWall('s-wall',0)}
-        {this.renderWall('h-wall',0)}
-        {this.renderWall('s-wall',0)}
-        {this.renderWall('h-wall',0)}
-        {this.renderWall('s-wall',0)}
-        {this.renderWall('h-wall',0)}
-        {this.renderWall('s-wall',0)}
-        {this.renderWall('h-wall',0)}
-        {this.renderWall('s-wall',0)}
-        {this.renderWall('h-wall',0)}
+        {this.renderWall('h-wall',i*(8+9)+0+8)}
+        {this.renderWall('s-wall')}
+        {this.renderWall('h-wall',i*(8+9)+1+8)}
+        {this.renderWall('s-wall')}
+        {this.renderWall('h-wall',i*(8+9)+2+8)}
+        {this.renderWall('s-wall')}
+        {this.renderWall('h-wall',i*(8+9)+3+8)}
+        {this.renderWall('s-wall')}
+        {this.renderWall('h-wall',i*(8+9)+4+8)}
+        {this.renderWall('s-wall')}
+        {this.renderWall('h-wall',i*(8+9)+5+8)}
+        {this.renderWall('s-wall')}
+        {this.renderWall('h-wall',i*(8+9)+6+8)}
+        {this.renderWall('s-wall')}
+        {this.renderWall('h-wall',i*(8+9)+7+8)}
+        {this.renderWall('s-wall')}
+        {this.renderWall('h-wall',i*(8+9)+8+8)}
       </div>
     )
   }
@@ -78,21 +78,21 @@ class defineBoard extends Component {
     return (
       <div>
         {this.renderBoardRow(0)}
-        {this.renderWallRow()}
+        {this.renderWallRow(0)}
         {this.renderBoardRow(1)}
-        {this.renderWallRow()}
+        {this.renderWallRow(1)}
         {this.renderBoardRow(2)}
-        {this.renderWallRow()}
+        {this.renderWallRow(2)}
         {this.renderBoardRow(3)}
-        {this.renderWallRow()}
+        {this.renderWallRow(3)}
         {this.renderBoardRow(4)}
-        {this.renderWallRow()}
+        {this.renderWallRow(4)}
         {this.renderBoardRow(5)}
-        {this.renderWallRow()}
+        {this.renderWallRow(5)}
         {this.renderBoardRow(6)}
-        {this.renderWallRow()}
+        {this.renderWallRow(6)}
         {this.renderBoardRow(7)}
-        {this.renderWallRow()}
+        {this.renderWallRow(7)}
         {this.renderBoardRow(8)}
       </div>
     )
--- src/js/components/Game.js
+++ src/js/components/Game.js
@@ -1,36 +1,19 @@
 import React, { Component } from "react"
-import { connect } from 'react-redux'
 import Board from './Board'
 
-const mapStateToProps = (state) => {
-  return {
-    blackIsNext: state.blackIsNext,
-    winner: state.winner
-  }
-}
-
-
-class defineGame extends Component {
+class Game extends Component {
   render(){
-    let status
-    if (this.props.winner){
-      status = 'Winner: '+ this.props.winner
-    } else {
-      status = 'Next player: ' + (this.props.blackIsNext ? 'Black' : 'White');
-    }
     return (
       <div className="game">
         <div className="game-board">
           <Board />
         </div>
         <div className="game-info">
-          <div>{status}</div>
+          <div>{/*status*/}</div>
         </div>
       </div>
     )
   }
 }
-
-const Game = connect(mapStateToProps)(defineGame);
 
 export default Game
